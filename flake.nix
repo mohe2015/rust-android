185ce7b419197bb0bb64492870689ca533f30bd8
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-10-15 23:51:43 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -54,8 +54,10 @@
           APK_DESTINATION=$(mktemp --suffix .zip)
           rm "$APK_DESTINATION"
 
-          ${pkgs.jdk}/bin/javac -d "$CLASSES" -classpath ${packages.x86_64-linux.android-jar} ${./src/de/selfmade4u/rust}/*.java
-          ${packages.x86_64-linux.buildTools}/libexec/android-sdk/build-tools/36.0.0/d8 $CLASSES/*.class --output "$APK_SOURCE"
+          ${pkgs.jdk}/bin/javac -d "$CLASSES" -classpath ${packages.x86_64-linux.android-jar} ${./src/de/selfmade4u/rust}/*.java ${./src/de/selfmade4u/rust}/**/*.java
+          ${packages.x86_64-linux.buildTools}/libexec/android-sdk/build-tools/36.0.0/d8 $CLASSES/*.class $CLASSES/**/*.class --output "$APK_SOURCE"
+          ls -R $APK_SOURCE
+          ls -R $CLASSES
 
           ${packages.x86_64-linux.buildTools}/libexec/android-sdk/build-tools/36.0.0/aapt2 link --output-to-dir -o "$APK_SOURCE" -I ${packages.x86_64-linux.android-jar} --manifest ${./AndroidManifest.xml}
 
