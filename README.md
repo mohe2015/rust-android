46c1984b43ebc118dddd09b10d7103af97e5f8b1
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-10-15 21:18:46 +0200

    wip

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ https://developer.android.com/build/building-cmdline#build_bundle
 aapt2 may also be able to build the android manifest?
 
 https://r8.googlesource.com/r8/+/refs/heads/main/README.md
+
+https://developer.android.com/guide/topics/manifest/manifest-intro
+
+https://android.googlesource.com/platform/manifest/+/refs/heads/android-latest-release/default.xml
+
+<default revision="android16-release" />
+
+API level 36
+
+https://android.googlesource.com/platform/prebuilts/sdk/+/refs/heads/android16-release/36/public/android.jar
+
+search for prebuilts/sdk
+
+https://android.googlesource.com/platform/prebuilts/sdk/+/refs/heads/android16-release
--- flake.nix
+++ flake.nix
@@ -8,26 +8,39 @@
   outputs =
     { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
+    in rec {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
 
       packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
       packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-      # nix run .#build-apk
-      packages.x86_64-linux.build-apk = nixpkgs.legacyPackages.x86_64-linux.writeShellApplication {
-        name = "build-apk";
-        text = ''
+      # ~/Android/Sdk/platforms/android-36/android.jar
+      packages.x86_64-linux.android-jar = let
+        base64 = pkgs.fetchurl {
+        url = "https://android.googlesource.com/platform/prebuilts/sdk/+/refs/heads/android16-release/36/public/android.jar?format=TEXT";
+        hash = "sha256-ZFX0cUU6/cfdeq5RR5nfZhsyqDvmvqPkvWsjdBl93JE=";
+      }; in pkgs.runCommandLocal "android.jar" {} ''
+        base64 -d ${base64} > $out
+      '';
+
+      packages.x86_64-linux.apk = pkgs.runCommand "result.apk" {} ''
           set -ex
           APK_SOURCE=$(mktemp -d)
           APK_DESTINATION=$(mktemp --suffix .zip)
           rm "$APK_DESTINATION"
 
-          ${pkgs.aapt}/bin/aapt2 link --output-to-dir -o "$APK_SOURCE" --manifest ${./AndroidManifest.xml}
+          ${pkgs.aapt}/bin/aapt2 link --output-to-dir -o "$APK_SOURCE" -I ${packages.x86_64-linux.android-jar} --manifest ${./AndroidManifest.xml}
 
           (cd "$APK_SOURCE" && ${pkgs.zip}/bin/zip -r "$APK_DESTINATION" .)
-          cp "$APK_DESTINATION" result.apk
+          cp "$APK_DESTINATION" $out
+      '';
+
+      # nix run .#build-apk
+      packages.x86_64-linux.build-apk = nixpkgs.legacyPackages.x86_64-linux.writeShellApplication {
+        name = "build-apk";
+        text = ''
+          cp ${packages.x86_64-linux.apk} result.apk
         '';
       };
     };
