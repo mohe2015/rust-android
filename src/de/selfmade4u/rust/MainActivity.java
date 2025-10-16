44858fbf7835b715e916511fa6544a0e6c428e7f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-10-16 13:24:22 +0200

    run apk

--- src/de/selfmade4u/rust/MainActivity.java
+++ src/de/selfmade4u/rust/MainActivity.java
@@ -1,5 +1,13 @@
 package de.selfmade4u.rust;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 public class MainActivity extends android.app.Activity {
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(new Button(getApplicationContext()));
+    }
 }
